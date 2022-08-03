ee386456cd16ee164f84ace910f0461fd675e3cf
sufyaninovio <105053286+sufyaninovio@users.noreply.github.com>
2022-08-03 09:04:40 -0700

    header(responsive)

--- inc/footerlinks.php
+++ inc/footerlinks.php
@@ -6,44 +6,54 @@
 
 <!-- Jquery JS -->
 <script>
-    $(document).ready(function () {
-        $(window).on('load', function(){
-            $("#preloader").fadeOut(1000);
-        });
-    })
-    $('.slide-this').slick({
-  dots: true,
-  infinite: false,
-  speed: 300,
-  slidesToShow: 4,
-  slidesToScroll: 4,
-  responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        infinite: true,
-        dots: true
+  $(document).ready(function() {
+    $(window).on('load', function() {
+      $("#preloader").fadeOut(1000);
+    });
+  })
+  // Mobile Nav
+  $('document').ready(function() {
+    $('#header .canvas-icon i').click(function() {
+      $("#header .mobile-header").addClass('show');
+    });
+
+    $('#header .mobile-header .cancel').click(function() {
+      $("#header .mobile-header").removeClass('show');
+    });
+  });
+  // Mobile Nav
+  $('.slide-this').slick({
+    dots: true,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    responsive: [{
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
       }
-    },
-    {
-      breakpoint: 600,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2
-      }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1
-      }
-    }
-    // You can unslick at a given breakpoint now by adding:
-    // settings: "unslick"
-    // instead of a settings object
-  ]
-});
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
 </script>
--- inc/header.php
+++ inc/header.php
@@ -1,9 +1,9 @@
 <header class="header">
     <div class="container">
         <div class="row">
-            <div class="col-md-4"></div>
-            <div class="col-md-4"></div>
-            <div class="col-md-4">
+            <!-- <div class="col-md-4"></div>
+            <div class="col-md-4"></div> -->
+            <div class="col-xl-4 col-lg-5 col-md-0 ml-auto d-none-991">
                 <div class="row">
                     <div class="col-md-4">
                         <div class="contact-detail">
@@ -35,45 +35,71 @@
                     </div>
                     <div class="col-md-3 p-0">
                         <div class="icons">
-                        <i class="fa fa-facebook-square"></i>
-                        <i class="fa fa-twitter-square"></i>
-                        <i class="fa fa-tripadvisor"></i>
+                            <i class="fa fa-facebook-square"></i>
+                            <i class="fa fa-twitter-square"></i>
+                            <i class="fa fa-tripadvisor"></i>
                         </div>
                     </div>
                 </div>
             </div>
-            <div class="col-md-4">
+        </div>
+        <div class="row">
+            <div class="col-md-4 responsive_nav_class">
                 <div class="header-nav">
-           <nav class="nabar">
-            <ul>
-                <li><a href="">home</a></li>
-                <li><a href="">About us </a></li>
-                <li><a href="">Activities </a></li>
-                <li><a href="">Features</a></li>
-            </ul>
-           </nav>
+                    <nav class="nabar">
+                        <ul>
+                            <li><a href="">home</a></li>
+                            <li><a href="">About us </a></li>
+                            <li><a href="">Activities </a></li>
+                            <li><a href="">Features</a></li>
+                        </ul>
+                    </nav>
+                </div>
             </div>
-            </div>
-            <div class="col-md-4 text-center">
-                <div class="col-md-12">
+            <div class="col-4 text-center">
                 <div class="logo-box">
                     <img src="img/logo.png" alt="" class="img-fluid">
                 </div>
             </div>
-            </div>
 
-            <div class="col-md-4">
-            <div class="header-nav">
-            <nav>
-            <ul>
-                <li><a href="">News</a></li>
-                <li><a href=""> Gallery</a></li>
-                <li><a href="">FAQ's </a></li>
-                <li><a href=""> Contact us</a></li>
-            </ul>
-           </nav>
-            </div>
+            <div class="col-md-4 responsive_nav_class">
+                <div class="header-nav">
+                    <nav>
+                        <ul>
+                            <li><a href="">News</a></li>
+                            <li><a href=""> Gallery</a></li>
+                            <li><a href="">FAQ's </a></li>
+                            <li><a href=""> Contact us</a></li>
+                        </ul>
+                    </nav>
+                </div>
             </div>
         </div>
+    </div>
+</header>
+<header id="header">
+    <div class="canvas-icon text-right">
+        <i class="fa fa-bars" aria-hidden="true"></i>
+    </div>
+    <!-- Nav section  -->
+
+    <div class="mobile-header navbar-dark">
+        <div class="cancel">
+            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-x"
+                viewBox="0 0 16 16">
+                <path
+                    d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
+            </svg>
+        </div>
+        <ul class="mobile-nav navbar-nav">
+            <li class="normal-nav-items nav-link"><a href="index.php">HOME</a></li>
+            <li class="normal-nav-items nav-link"><a href="about.php">ABOUT US</a></li>
+            <li class="normal-nav-items nav-link"><a href="product.php">Activities</a></li>
+            <li class="normal-nav-items nav-link"><a href="product.php">Features</a></li>
+            <li class="normal-nav-items nav-link"><a href="product.php">News</a></li>
+            <li class="normal-nav-items nav-link"><a href="product.php">Gallery</a></li>
+            <li class="normal-nav-items nav-link"><a href="product.php">FAQ's </a></li>
+            <li class="normal-nav-items nav-link"><a href="product.php">Contact us</a></li>
+        </ul>
     </div>
 </header>
